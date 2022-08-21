7d7bd499f0cd4ee4fa3a261be90980765d4067bf
unknown <100776483+yuna-haru@users.noreply.github.com>
2022-08-21 16:03:25 +0900

    [finish]5章

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000ff',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp(1000);
+  });
+  
 });
